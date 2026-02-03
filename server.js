b523addd0b8923c93d3815d55de7d391395867f5
Aditya Pratap Singh <adityaprataplda@gmail.com>
2026-02-03 21:23:03 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -79,6 +79,6 @@ app.get('/resources', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
